d4a0fdab1bef4108206e43d00351519b6984ba56
JackAKirk <chezjakirk@gmail.com>
2021-08-31 11:09:49 +0100

    Added complete example minus SFINAE impl.

--- sycl/include/sycl/ext/oneapi/matrix/matrix-cuda.hpp
+++ sycl/include/sycl/ext/oneapi/matrix/matrix-cuda.hpp
@@ -7,7 +7,6 @@ __SYCL_INLINE_NAMESPACE(cl) {
 namespace sycl {
 namespace ext {
 namespace intel {
-// namespace detail {
 
 namespace experimental::matrix {
 
@@ -16,80 +15,187 @@ enum class matrix_type { a, b, c };
 enum class matrix_layout { row_major, col_major, packed };
 
 // todo deal with Conditional enable_ifs for each case if appropriate.
-template <typename Group, typename T, matrix_type MT,
-          size_t Rows = sycl::dynamic_extent,
+template <typename Group, matrix_type MT, size_t Rows = sycl::dynamic_extent,
           size_t Cols = sycl::dynamic_extent,
           matrix_layout Layout = matrix_layout::row_major>
 struct joint_matrix {
   joint_matrix(Group g) {}
 };
 
+// TODO are A/B really identical in this case?
 template <matrix_layout Layout>
-struct joint_matrix<sub_group, int8_t, matrix_type::a, 16, 16, Layout> {
+struct joint_matrix<sub_group, matrix_type::a, 16, 16, Layout> {
   int32_t data[2]; // "s8" : "i32" and "m16n16k16:a:s8" : 2
 };
 
 template <matrix_layout Layout>
-struct joint_matrix<sub_group, int8_t, matrix_type::b, 16, 16, Layout> {
+struct joint_matrix<sub_group, matrix_type::b, 16, 16, Layout> {
   int32_t data[2]; // "s8" : "i32" and "m16n16k16:b:s8" : 2
 };
 
 template <matrix_layout Layout>
-struct joint_matrix<sub_group, int, matrix_type::c, 16, 16, Layout> {
+struct joint_matrix<sub_group, matrix_type::c, 16, 16, Layout> {
   int32_t data[8]; // "s32" : "i32" and "m16n16k16:c:s32" : 8
 };
 
-template <typename Group, typename T, matrix_type MT, size_t NumRows,
-          size_t NumCols, matrix_layout Layout, access::address_space Space>
+} // namespace experimental::matrix
+
+namespace detail {
+using namespace experimental;
+
+template <typename Group, matrix::matrix_type MT, size_t NumRows,
+          size_t NumCols, matrix::matrix_layout Layout,
+          access::address_space Space>
 struct joint_matrix_load_impl {
-  void load(joint_matrix<Group, T, MT, NumRows, NumCols, Layout> &res,
-            multi_ptr<T, Space> src, size_t stride);
+  void load(matrix::joint_matrix<Group, MT, NumRows, NumCols, Layout> &res,
+            multi_ptr<int32_t, Space> src, size_t stride);
 };
 
-template <matrix_layout Layout, access::address_space Space>
-struct joint_matrix_load_impl<sub_group, int8_t, matrix_type::a, 16, 16, Layout,
+// Helper
+template <matrix::matrix_layout Layout> const int get_layout_int() {
+  static_assert(Layout == matrix::matrix_layout::row_major ||
+                Layout == matrix::matrix_layout::col_major);
+  return Layout == matrix::matrix_layout::row_major ? 0 : 1;
+}
+
+// TODO need to add a conditional using matrix layout.
+// std::enable_if_t<Layout == matrix_layout::row_major || Layout ==
+// matrix_layout::col_major, int> = 0>
+template <matrix::matrix_layout Layout, access::address_space Space>
+struct joint_matrix_load_impl<sub_group, matrix::matrix_type::a, 16, 16, Layout,
                               Space> {
-  void
-  load(joint_matrix<sub_group, int8_t, matrix_type::a, 16, 16, Layout> &res,
-       multi_ptr<int8_t, Space> src, size_t stride) {
-    __imma_m16n16k16_ld_a_s8(res.data, src, stride, 0);
+  void load(matrix::joint_matrix<sub_group, matrix::matrix_type::a, 16, 16,
+                                 Layout> &res,
+            multi_ptr<int32_t, Space> src, size_t stride) {
+    // const int i = 0;
+    // const int* ll;
+    __imma_m16n16k16_ld_a_s8(res.data, src.get(), stride,
+                             static_cast<const int>(get_layout_int<Layout>()));
   }
 };
 
-template <matrix_layout Layout, access::address_space Space>
-struct joint_matrix_load_impl<sub_group, int8_t, matrix_type::b, 16, 16, Layout,
+template <matrix::matrix_layout Layout, access::address_space Space>
+struct joint_matrix_load_impl<sub_group, matrix::matrix_type::b, 16, 16, Layout,
                               Space> {
-  void
-  load(joint_matrix<sub_group, int8_t, matrix_type::b, 16, 16, Layout> &res,
-       multi_ptr<int8_t, Space> src, size_t stride) {
-    __imma_m16n16k16_ld_b_s8(res.data, src, stride, 0);
+  void load(matrix::joint_matrix<sub_group, matrix::matrix_type::b, 16, 16,
+                                 Layout> &res,
+            multi_ptr<int32_t, Space> src, size_t stride) {
+    // const int i = 0;
+    // const int* ll;
+    __imma_m16n16k16_ld_b_s8(res.data, src.get(), stride, 0);
   }
 };
 
 // get_layout_int<Layout>() replaced with 0 in final buildin argument;
-template <matrix_layout Layout, access::address_space Space>
-struct joint_matrix_load_impl<sub_group, int32_t, matrix_type::c, 16, 16,
-                              Layout, Space> {
-  void
-  load(joint_matrix<sub_group, int32_t, matrix_type::c, 16, 16, Layout> &res,
-       multi_ptr<int32_t, Space> src, size_t stride) {
-    __imma_m16n16k16_ld_c(res.data, src, stride, 0);
+template <matrix::matrix_layout Layout, access::address_space Space>
+struct joint_matrix_load_impl<sub_group, matrix::matrix_type::c, 16, 16, Layout,
+                              Space> {
+  void load(matrix::joint_matrix<sub_group, matrix::matrix_type::c, 16, 16,
+                                 Layout> &res,
+            multi_ptr<int32_t, Space> src, size_t stride) {
+    // const int i = 0;
+    __imma_m16n16k16_ld_c(res.data, src.get(), stride, 0);
   }
 };
 
-template <typename Group, typename T, matrix_type MT, size_t NumRows,
-          size_t NumCols, matrix_layout Layout, access::address_space Space>
-void joint_matrix_load(
-    Group sg, joint_matrix<Group, T, MT, NumRows, NumCols, Layout> &res,
-    multi_ptr<T, Space> src, size_t stride,
+template <typename Group, size_t NumRows, size_t NumCols,
+          matrix::matrix_layout Layout, access::address_space Space>
+struct joint_matrix_store_impl {
+  void store(matrix::joint_matrix<Group, matrix::matrix_type::c, NumRows,
+                                  NumCols, Layout> &src,
+             multi_ptr<int32_t, Space> dst, size_t stride);
+};
+
+// todo: assuming row order.
+template <matrix::matrix_layout Layout, access::address_space Space>
+struct joint_matrix_store_impl<sub_group, 16, 16, Layout, Space> {
+  void store(matrix::joint_matrix<sub_group, matrix::matrix_type::c, 16, 16,
+                                  Layout> &src,
+             multi_ptr<int32_t, Space> dst, size_t stride) {
+    __imma_m16n16k16_st_c_i32(dst, src.data, stride, 0);
+  }
+};
+template <typename Group, std::size_t M, std::size_t K, std::size_t N,
+          matrix::matrix_layout LayoutA, matrix::matrix_layout LayoutB,
+          matrix::matrix_layout LayoutC>
+struct joint_matrix_mma_impl {
+  matrix::joint_matrix<Group, matrix::matrix_type::c, M, N, LayoutC>
+  mma(Group sg,
+      matrix::joint_matrix<Group, matrix::matrix_type::a, M, K, LayoutA> A,
+      matrix::joint_matrix<Group, matrix::matrix_type::b, K, N, LayoutB> B,
+      matrix::joint_matrix<Group, matrix::matrix_type::c, M, N, LayoutC> C);
+};
+
+// Helper
+template <matrix::matrix_layout LayoutA, matrix::matrix_layout LayoutB>
+int get_layout_comb_int() {
+  static_assert(LayoutA == matrix::matrix_layout::row_major ||
+                LayoutA == matrix::matrix_layout::col_major);
+  static_assert(LayoutB == matrix::matrix_layout::row_major ||
+                LayoutB == matrix::matrix_layout::col_major);
+  if (LayoutA == matrix::matrix_layout::row_major)
+    return LayoutB == matrix::matrix_layout::row_major ? 0 : 1;
+  // LayoutA == matrix_layout::col_major must hold here
+  return LayoutB == matrix::matrix_layout::row_major ? 2 : 3;
+}
+
+template <matrix::matrix_layout LayoutA, matrix::matrix_layout LayoutB,
+          matrix::matrix_layout LayoutC>
+struct joint_matrix_mma_impl<sub_group, 16, 16, 16, LayoutA, LayoutB, LayoutC> {
+  matrix::joint_matrix<sub_group, matrix::matrix_type::c, 16, 16, LayoutC>
+  mma(sub_group sg,
+      matrix::joint_matrix<sub_group, matrix::matrix_type::a, 16, 16, LayoutA>
+          A,
+      matrix::joint_matrix<sub_group, matrix::matrix_type::b, 16, 16, LayoutB>
+          B,
+      matrix::joint_matrix<sub_group, matrix::matrix_type::c, 16, 16, LayoutC>
+          C) {
+    matrix::joint_matrix<sub_group, matrix::matrix_type::c, 16, 16, LayoutC> D;
+    __imma_m16n16k16_mma_s8(D.data, A.data, B.data, C.data, 0, 0);
+    return D;
+  }
+};
+
+} // namespace detail
+
+namespace experimental::matrix {
+template <typename Group, matrix_type MT, size_t NumRows, size_t NumCols,
+          matrix_layout Layout, access::address_space Space>
+void joint_matrix_load(Group sg,
+                       joint_matrix<Group, MT, NumRows, NumCols, Layout> &res,
+                       multi_ptr<int32_t, Space> src, size_t stride,
+                       matrix_layout layout = matrix_layout::row_major) {
+  // Should be a requirement for the CUDA backend
+  assert(layout == Layout);
+  detail::joint_matrix_load_impl<Group, MT, NumRows, NumCols, Layout, Space>{}
+      .load(res, src, stride);
+}
+
+template <typename Group, size_t NumRows, size_t NumCols, matrix_layout Layout,
+          access::address_space Space>
+void joint_matrix_store(
+    Group sg,
+    joint_matrix<Group, matrix_type::c, NumRows, NumCols, Layout> &src,
+    multi_ptr<int32_t, Space> dst, size_t stride,
     matrix_layout layout = matrix_layout::row_major) {
   // Should be a requirement for the CUDA backend
   assert(layout == Layout);
-  joint_matrix_load_impl<Group, T, MT, NumRows, NumCols, Layout, Space>{}.load(
-      res, src, stride);
+  detail::joint_matrix_store_impl<Group, NumRows, NumCols, Layout, Space>{}
+      .store(src, dst, stride);
 }
 
+template <typename Group, std::size_t M, std::size_t K, std::size_t N,
+          matrix_layout LayoutA, matrix_layout LayoutB, matrix_layout LayoutC>
+joint_matrix<Group, matrix_type::c, M, N, LayoutC>
+joint_matrix_mad(Group sg, joint_matrix<Group, matrix_type::a, M, K, LayoutA> A,
+                 joint_matrix<Group, matrix_type::b, K, N, LayoutB> B,
+                 joint_matrix<Group, matrix_type::c, M, N, LayoutC> C) {
+  return detail::joint_matrix_mma_impl<Group, M, K, N, LayoutA, LayoutB,
+                                       LayoutC>{}
+      .mma(sg, A, B, C);
+}
 } // namespace experimental::matrix
+
 } // namespace intel
 } // namespace ext
 } // namespace sycl
--- sycl/test/matrix/matrix_cuda.cpp
+++ sycl/test/matrix/matrix_cuda.cpp
@@ -3,33 +3,88 @@
 using namespace sycl;
 using namespace sycl::ext::intel::experimental::matrix;
 
+// This matrix size only requires a single subgroup operation: The "big matrix"
+// matches the size of a tile "sub matrix" for A,B,C: i.e. N=M=K=n=m=k=16.
 #define x 16
 #define y 16
 
+// constexpr int M = 16;
+// constexpr int N = 16;
+// constexpr int K = 16;
+
+constexpr int N_THREADS_PER_MATRIX_OP = 32;
+constexpr int N_TILES = 1;
+
+// The stride should equal the leading dimension of the "big matrix" matrix I
+// think.
+constexpr int STRIDE = 16;
+
 int main() {
 
-  int C[256];
+  int32_t C[256];
+  int32_t A[256];
+  int32_t B[256];
 
-  for (auto &c : C) {
-    c = 0;
+  for (int i = 0; i < 256; i++) {
+    A[i] = 0;
+    B[i] = 0;
+    C[i] = 0;
   }
 
-  buffer<int, 2> bufC(C, range<2>(16, 16));
+  // input data appears to the same for each matrix in this case...
+  buffer<int32_t, 2> bufC(C, range<2>(x, y));
+  buffer<int32_t, 2> bufA(A, range<2>(x, y));
+  buffer<int32_t, 2> bufB(B, range<2>(x, y));
 
   queue q;
   q.submit([&](handler &cgh) {
     auto accC = bufC.get_access<access::mode::read_write>(cgh);
+    auto accA = bufA.get_access<access::mode::read_write>(cgh);
+    auto accB = bufB.get_access<access::mode::read_write>(cgh);
 
     cgh.parallel_for<class imatrix>(
-        nd_range<2>({1, 32}, {1, 32}), [=](nd_item<2> item)
+        nd_range<2>({N_TILES, N_THREADS_PER_MATRIX_OP},
+                    {N_TILES, N_THREADS_PER_MATRIX_OP}),
+        [=](nd_item<2> item)
 
         {
           ext::oneapi::sub_group sg = item.get_sub_group();
-          joint_matrix<sub_group, int, matrix_type::c, x, y,
+          joint_matrix<sub_group, matrix_type::c, x, y,
                        matrix_layout::row_major>
               sub_c;
-          //__imma_m16n16k16_ld_c(sub_c.data, accC.get_pointer(), 23, 1);
-          // joint_matrix_load(sg, sub_c, accC.get_pointer(), 23);
+
+          joint_matrix<sub_group, matrix_type::a, x, y,
+                       matrix_layout::row_major>
+              sub_a;
+
+          joint_matrix<sub_group, matrix_type::b, x, y,
+                       matrix_layout::row_major>
+              sub_b;
+
+          // Note that matrix "C" here is acting as matrices C,D in ptx
+          // terminology. It is added to the matrix product of A and B. calls:
+          //__imma_m16n16k16_ld_c(sub_c.data, accC.get_pointer(), 16, 0);
+          joint_matrix_load(sg, sub_c, accC.get_pointer(), 16);
+
+          // TODO There is a seemingly notintuitive loop consisting of K / k
+          // iterations in the AMX tests. At first glance this looks like the
+          // A/B matrices are loaded one row/column at a time and a row of
+          // matrix A is multiplied by a column of matrix B at a time.  But this
+          // idea doesn't match the implementation.  Hopefully we can ignore
+          // this detail until there is at least a working implementation for a
+          // single matrix tile.
+
+          // e.g. for (int i = 0; i < K/k; i++)
+          {
+            joint_matrix_load(sg, sub_a, accA.get_pointer(), 16);
+            //__imma_m16n16k16_ld_a_s8(sub_a.data, accA.get_pointer(), 16, 0);
+            joint_matrix_load(sg, sub_b, accB.get_pointer(), 16);
+            //__imma_m16n16k16_ld_b_s8(sub_b.data, accB.get_pointer(), 16, 0);
+
+            joint_matrix_mad(sg, sub_a, sub_b, sub_c);
+          }
+
+          joint_matrix_store(sg, sub_c, accC.get_pointer(), 16);
         });
   });
 
